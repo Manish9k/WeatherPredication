34007d442f7f8ab9ae6ec23e9f01456f9585ec21
Manish Kumar <60013893+Manish9k@users.noreply.github.com>
2022-08-06 13:21:49 +0530

    Add files via upload

---
+++ wscript.js
@@ -0,0 +1,20 @@
+ let weather = {
+    apiKey: "b41fd95acd4775a99c96c19c848c3a8a",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      
